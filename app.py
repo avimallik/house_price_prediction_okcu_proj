4784b49e3fdb0d865c25583a1dad125b65d1958f
avimallik <armavicse@gmail.com>
2025-04-16 12:18:25 +0600

    imported MySql package and integrate HTTP POST request

--- app.py
+++ app.py
@@ -23,11 +23,7 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO hpp_user VALUES(NULL, %s, %s, %s)''',(username, password, email))
-        mysql.connection.commit()
-        cursor.close()
-        msg = 'You have successfully registered, Please back to the Login page !'
+    
     return render_template('register.html', msg=msg)
 
 
